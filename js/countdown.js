0c4fb9372be52426fb3b5833690da733f31d8ee7
rcagantas <roel.cagantas@gmail.com>
2020-02-28 18:18:30 +0800

    add countdown

---
+++ js/countdown.js
@@ -0,0 +1,21 @@
+const countdownDate = new Date('Jan 30, 2021 12:00:00').getTime();
+
+let intervalHandler = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = 
+        days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "IT'S HAPPENING!";
+    }
+
+}, 1000);
